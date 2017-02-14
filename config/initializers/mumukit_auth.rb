309708d3f9c135ecf409a29a529cc935633d6a55
Franco Bulgarelli <franco@mumuki.org>
2017-02-14 00:44:30 -0300

    Removing dead code

--- config/initializers/mumukit_auth.rb
+++ config/initializers/mumukit_auth.rb
@@ -3,8 +3,3 @@ class Mumukit::Auth::Permissions
     scope_for(:student)&.grants&.map { |grant| grant.instance_variable_get(:@first) }.to_set + [Organization.central]
   end
 end
-
-Mumukit::Auth.configure do |c|
-  # We are not using tokens, so implementing this strategy is meaningless
-  c.persistence_strategy = nil
-end
