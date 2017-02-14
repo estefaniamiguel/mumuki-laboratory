b0b278ada73ba002a9093b725b9f03b5caa32a74
Franco Bulgarelli <franco@mumuki.org>
2017-02-14 17:20:01 -0300

    DRY up router

--- config/routes.rb
+++ config/routes.rb
@@ -10,30 +10,28 @@ Rails.application.routes.draw do
   # Current user
   resources :comments, only: :index
 
-  resources :organizations, only: :index
-
-  scope '/:organization' do
-    resources :book, as: 'organization_book', only: [:show]
-    resources :chapters, as: 'organization_chapters', only: [:show] do
+  scope '/:organization', as: 'organization' do
+    resources :book, only: [:show]
+    resources :chapters, only: [:show] do
       resource :appendix, only: :show
     end
 
     # All users
-    resources :exercises, as: 'organization_exercises', only: [:show, :index] do
+    resources :exercises, only: [:show, :index] do
       # Current user
       resources :solutions, controller: 'exercise_solutions', only: :create
       resources :queries, controller: 'exercise_query', only: :create
     end
 
     # All users
-    resources :guides, as: 'organization_guides', only: [:show, :index]
-    resources :lessons, as: 'organization_lesson', only: [:show]
-    resources :complements, as: 'organization_complements', only: [:show]
-    resources :exams, as: 'organization_exams', only: [:show]
+    resources :guides, only: [:show, :index]
+    resources :lessons, only: [:show]
+    resources :complements, only: [:show]
+    resources :exams, only: [:show]
 
     # Routes by slug
-    get '/guides/:first/:second' => 'guides#show_by_slug', as: :organization_guide_by_slug
-    get '/exercises/:first/:second/:bibliotheca_id' => 'exercises#show_by_slug', as: :organization_exercise_by_slug
+    get '/guides/:first/:second' => 'guides#show_by_slug', as: :guide_by_slug
+    get '/exercises/:first/:second/:bibliotheca_id' => 'exercises#show_by_slug', as: :exercise_by_slug
   end
 
   #Rescue not found routes
